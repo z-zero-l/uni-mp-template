d799b29dd3024f7434477fb02a2794fbeae4584b
zero <z.zero.l@outlook.com>
2023-11-07 10:31:34 +0800

    安装windicss&封装http.ts

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,8 +5,15 @@
 <template>
   <view class="index">index</view>
   <div class="ellipsis">哪里贵了? 这么多年都是这个价格</div>
+  <div class="flex justify-center h-100 items-center">
+    <div class="bg-red-300 h-10 w-12"></div>
+  </div>
+  <div class="test bg-yellow-300 w-700 h-100"></div>
 </template>
 
 <style lang="scss">
 //
+.test {
+  width: 749rpx;
+}
 </style>
--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 //
+const foo = ref('test')
 </script>
 
 <template>
