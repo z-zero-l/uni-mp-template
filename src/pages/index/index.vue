43d6355a6f553a09372f8ba17f2e25b09da059ac
zero <z.zero.l@outlook.com>
2023-11-18 11:04:04 +0800

    首页模块

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,19 +1,102 @@
 <script setup lang="ts">
-//
-</script>
+import CustomNavbar from './components/CustomNavbar.vue'
+import wSwiper from '@/components/swiper/wSwiper.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
+import HotPannel from './components/HotPannel.vue'
+import Guess from '@/components/guess/Guess.vue'
+import type { GuessInstance } from '@/types/component'
+import PageSkeleton from './components/PageSkeleton.vue'
 
+// 是否加载中标记
+const isLoading = ref(false)
+
+onLoad(async () => {
+  isLoading.value = true
+  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()])
+  isLoading.value = false
+})
+
+// 获取轮播图
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+
+// 获取分类数据
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+}
+
+// 获取热门推荐
+const hotList = ref<HotItem[]>([])
+const getHomeHotData = async () => {
+  const res = await getHomeHotAPI()
+  hotList.value = res.result
+}
+
+// 获取猜你喜欢组件实例
+const guessRef = ref<GuessInstance>()
+// 滚动触底事件
+const onScrolltolower = () => {
+  guessRef.value?.getMore()
+}
+
+// 下拉刷新状态
+const isTriggered = ref(false)
+// 自定义下拉刷新被触发
+const onRefresherrefresh = async () => {
+  // 开启动画
+  isTriggered.value = true
+  // 重置猜你喜欢组件数据
+  guessRef.value?.resetData() // 加载数据
+  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()]) // 关闭动画
+  isTriggered.value = false
+}
+</script>
 <template>
-  <view class="index">index</view>
-  <div class="ellipsis">哪里贵了? 这么多年都是这个价格</div>
-  <div class="flex justify-center h-100 items-center">
-    <div class="bg-red-300 h-10 w-12"></div>
-  </div>
-  <div class="test bg-yellow-300 w-700 h-100"></div>
+  <view class="viewport">
+    <!-- 自定义导航栏 -->
+    <CustomNavbar></CustomNavbar>
+    <!-- 滚动容器 -->
+    <scroll-view
+      refresher-enabled
+      @refresherrefresh="onRefresherrefresh"
+      :refresher-triggered="isTriggered"
+      @scrolltolower="onScrolltolower"
+      class="scroll-view"
+      scroll-y
+    >
+      <PageSkeleton v-if="isLoading"></PageSkeleton>
+      <template v-else>
+        <wSwiper :list="bannerList"></wSwiper>
+        <!-- 商品分类 -->
+        <CategoryPanel :list="categoryList" />
+        <!-- 热门推荐 -->
+        <HotPannel :list="hotList" />
+        <!-- 猜你喜欢 -->
+        <Guess ref="guessRef" />
+      </template>
+    </scroll-view>
+  </view>
 </template>
 
 <style lang="scss">
-//
-.test {
-  width: 749rpx;
+page {
+  background-color: #f7f7f7;
+  height: 100%;
+  overflow: hidden;
+}
+/* 设置滚动区高度 */
+.viewport {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
+  overflow: hidden;
 }
 </style>
--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -1,13 +1,13 @@
 import 'vue'
-import wGuess from '@/components/wGuess/wGuess.vue'
-import wSwiper from '@/components/wSwiper/wSwiper.vue'
+import wGuess from '@/components/guess/Guess.vue'
+import wSwiper from '@/components/swiper/wSwiper.vue'
 
 declare module 'vue' {
   export interface GlobalComponents {
     wSwiper: typeof wSwiper
-    wGuess: typeof wGuess
+    Guess: typeof Guess
   }
 }
 
 // 组件实例类型
-export type wGuessInstance = InstanceType<typeof wGuess>
+export type GuessInstance = InstanceType<typeof Guess>
